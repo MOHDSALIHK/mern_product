58f3deb31ed7ffde0b23673dac9123a3a6f30010
ICT Academy <ict@mainlab14.ictkerala.org>
2023-05-26 16:51:42 +0530

    second commit

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,34 @@
-const express=require('express')
-const app=express()
-const cors=require('cors')
+const express = require('express'); //! to build backend quickly 
+const app = express(); //!instance of express
+const cors = require('cors'); //!for cross orgin policy
+const PORT = 8745
+app.use(cors()); //! "app.use(cors())" means use this cors package when server starts up
+
+app.use(express.json()); //! to parse json data
+app.use(express.urlencoded({extended:true})); //! to parse url encoded data
+app.use(express.static('public')); //! to serve static files
 
 
-app.use(cors())
-require('./dB')
+// DB 
+require('./dB') //!mongoose DB connection folder is called here hen server starts up
+const PRODUCT = require('./model/product')
 
-app.listen(4001,function(){
-    console.log("server stared");
+// To add product details coming from front end to backend database 
+app.post('/products',async (req,res)=>{
+    try {
+        console.log(req.body);
+        let item = req.body;
+
+        const saveItem = PRODUCT(item);
+        await saveItem.save();
+        res.send("Success!");
+
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`)
 })
--- backend/dB/index.js
+++ backend/dB/index.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-mongoose.connect('mongodb+srv://muhammedsalihk:Salih@cluster0.ti4qtlz.mongodb.net/')
+mongoose.connect('mongodb+srv://muhammedsalihk:Salih@cluster0.ti4qtlz.mongodb.net/Products')
 .then(()=>{
     console.log("db is connected");
 }).catch((err)=>{
